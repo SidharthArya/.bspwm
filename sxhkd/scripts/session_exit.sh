43c20f9c0273bb10ad6a8838d20eae4ec5ddbf6f
Arya <arya@archlinux.Archlinux>
2020-07-01 21:09:28 +0530

    Auto Commit: Wed 01 Jul 2020 09:09:28 PM IST

--- sxhkd/scripts/session_exit.sh
+++ sxhkd/scripts/session_exit.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+xdo kill -n firefox;
 for node in $(bspc query -N); do 
 	bspc node $node -c;
 done
 
+while [[ $(bspc query -N | wc -l) > 0 ]];
+do
+    sleep 5;
+done
+
+st -e sh ~/.local/scripts/sync.sh
