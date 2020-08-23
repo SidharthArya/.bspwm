9abc12ea5262884a5f33470d8a2b0f34e2ecd2bc
Arya <arya@archlinux.Archlinux>
2020-08-23 12:31:42 +0530

    Auto Commit: Sun 23 Aug 2020 12:31:40 PM IST

--- sxhkd/scripts/session_start.sh
+++ sxhkd/scripts/session_start.sh
@@ -5,6 +5,7 @@ gocryptfs -extpass 'kdialog --password Files' ~/Private/Drive/Personal ~/Documen
     emacs --config tracking & 
     st -e sh ~/.local/scripts/sync.sh &
     gwenview ~/Documents/Org/Tracking/images &
+    python ~/.local/scripts/electricitybill.py &
     ) ||
         (
             kdialog --msgbox "Hello Guest";
