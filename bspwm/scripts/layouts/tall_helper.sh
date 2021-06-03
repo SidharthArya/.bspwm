40f5e715d8e42daedece2561332cf509cbaee18b
Sidharth Arya <sidhartharya10@gmail.com>
2021-06-03 20:44:16 +0530

    Auto Commit: Thu Jun  3 08:44:16 PM IST 2021

--- bspwm/scripts/layouts/tall_helper.sh
+++ bspwm/scripts/layouts/tall_helper.sh
@@ -5,41 +5,53 @@ do
     case ${line%% *} in
         node_add)
             bspc node @/2 -B;
-            bspc node -s biggest.local;
+            bspc node -s biggest.local.\!hidden;
             echo Adding
             ;;
         node_remove)
-            bspc node @/ -i
-            bspc node focused -n $(bspc query -N -n .local.leaf.\!window);
+            count=$(bspc query -N -d focused -n .window.\!hidden.\!floating | wc -l)
+            bspc node @/2 -i
+            check=$(bspc query -T -n @/2  | jq .secondChild.hidden)
+            echo $check
+            if [[ $check == "true" ]] || [[ $check == "null" ]];
+            then
+                bspc node prev.leaf.\!window -k
+                bspc node focused -s biggest.local;
+            else
+                bspc node focused -s biggest.local;
+                [[ "$count" -eq "2" ]] && bspc node focused -n prev.leaf.\!window || bspc node prev.leaf.\!window -k
+                echo $count
+            fi
+            bspc node @/2 -B;
             echo Removing
             ;;
         node_flag)
             flag=$(echo $line | awk '{print $5}') 
             state=$(echo $line | awk '{print $6}')
             echo $flag $state
-            if [[ $flag == "hidden" ]];
-            then
-            WINDOWS=$(bspc query -N -d focused -n .window.leaf.\!hidden | wc -l)
-            echo $WINDOWS
-            case $state in
-                "on")
-                    if [[ $WINDOWS -gt 0 ]];
-                    then
-                    bspc node @/ -i
-                    bspc node focused -n $(bspc query -N -n .local.leaf.\!window);
-                    fi
-                    echo Removing
-                    ;;
-                "off")
-                    bspc node @/2 -i
-                    bspc node focused -n $(bspc query -N -n .local.leaf.\!window);
-                    bspc node @/2 -B;
-                    bspc node -s biggest.local;
-                    echo Unhiding
-            ;;
-            esac
+            # if [[ $flag == "hidden" ]];
+            # then
+            # WINDOWS=$(bspc query -N -d focused -n .window.leaf.\!hidden | wc -l)
+            # echo $WINDOWS
+            # case $state in
+            #     "on")
+            #         if [[ $WINDOWS -gt 0 ]];
+            #         then
+            #         bspc node @/ -i
+            #         bspc node focused -n $(bspc query -N -n .local.leaf.\!window);
+            #         fi
+            #         echo Removing
+            #         ;;
+            #     "off")
+            #         bspc node @/2 -i
+            #         bspc node focused -n $(bspc query -N -n .local.leaf.\!window);
+            #         bspc node @/2 -B;
+            #         bspc node -s biggest.local;
+            #         echo Unhiding
+            # ;;
+            # esac
 
-            fi
+            # fi
             ;;
             node_state)
             
@@ -50,16 +62,9 @@ do
             then
                 if [[ $state == "off" ]];
                 then
-                    if [[ $(bspc query -N -n) == $(bspc query -N -n @/1) ]];
-                    then
                     bspc node @/1 -i
-                    bspc node last.local.leaf.\!floating -n $(bspc query -N -n .local.leaf.\!window);
+                    bspc node prev.local.window.\!floating.\!hidden -n prev.leaf.\!window 
                     bspc node @/2 -B;
-                    bspc node last.local.leaf.\!floating -s biggest.local;
-                    else
-                    bspc node @/2 -B;
-                    fi
-                    
                 else
                     bspc node @/2 -i
                     bspc node focused -n $(bspc query -N -n .local.leaf.\!window);
@@ -69,4 +74,4 @@ do
             fi
             ;;
     esac
-done < <(bspc subscribe node_add node_remove node_state node_flag)
+done < <(bspc subscribe node_add node_remove node_state)
