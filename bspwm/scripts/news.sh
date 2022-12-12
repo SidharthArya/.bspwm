d336367a3d07b95c5ab863e6137402e4aaadb168
Sidharth Arya <code@sidhartharya.com>
2022-12-12 18:06:55 +0530

    Office Laptop

--- bspwm/scripts/news.sh
+++ bspwm/scripts/news.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-cd ~/.news/Feeds/
+st -e 'cd ~/.news/Feeds/
 export PATH="$PATH":~/.local/opt/google-cloud-sdk/bin
-git pull
+git pull'
 emacs --config news
+st -e 'cd ~/.news/Feeds/
 git add .
 git commit -m "Updated: $(date)"
-git push
+git push'
